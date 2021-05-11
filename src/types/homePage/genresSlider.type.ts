13ea2ad5f30baa74f3dc714195960baed9b0e55e
Володимир Некоз <kb191_nvv@student.ztu.edu.ua>
2021-05-11 20:03:19 +0300

    mainPage

--- src/types/homePage/genresSlider.type.ts
+++ src/types/homePage/genresSlider.type.ts
@@ -1,7 +1,7 @@
 import {selectorDataType} from "./selectorData.type";
 
 export type GenresSliderType = {
-    data: selectorDataType,
-    addGenre: (genre: number) => void,
+    data: selectorDataType
+    addGenre: (genre: number) => void
     removeGenre: (genre: number) => void
 }
